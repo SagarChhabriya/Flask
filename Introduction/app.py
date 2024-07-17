08c395395d0b956e6dd860fecca057a40f876f1d
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-07-18 00:49:29 +0500

    Created a simple flask web app

--- Introduction/app.py
+++ Introduction/app.py
@@ -9,6 +9,19 @@ def home():
     return "This is Home Page"
 
 
+# Add a path paramter in a page
+@app.route("/welcome/<name>")
+def welcom(name):
+    return f"Hello {name}, Welcome to our website!"
+
+# Input URL Ex: http://127.0.0.1:5000/welcome/ok
+# Output: Hello ok, Welcome to our website!
+
+
+# Add a integer path parameter
+@app.route("/add/<int:num1>/<int:num2>")
+def add(num1, num2):
+    return f"The sum of {num1} and {num2} is {num2+num1}"
 # Start the app
 if __name__ == "__main__":
     app.run(debug=True)
