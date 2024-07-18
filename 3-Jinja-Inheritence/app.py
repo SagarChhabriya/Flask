6bfb96888b650220fc0fcc49e22f8b4918ed74be
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-07-19 01:53:27 +0500

    Add layout.html for inheritence

--- 3-Jinja-Inheritence/app.py
+++ 3-Jinja-Inheritence/app.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template(("home.html"))
+    return render_template("home.html",title="Home")
+    # The title parameter is passed to the {{title}} variable in home.html --> layout.html
 
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title="About")
 
 if __name__=="__main__":
     app.run(debug=True)
