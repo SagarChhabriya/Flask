beb8d7b13e120feb4daf5389488e74184e97be40
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-07-21 19:04:44 +0500

    Add emp.py data file

---
+++ Practice/3-Jinja-Inheritence/emp.py
@@ -0,0 +1,13 @@
+employees_data = {
+ 1: {
+     "name": "Michael",
+     "age": 42,
+     "position": "Manager",
+ },
+ 2 : {
+     "name":"Sagar",
+     "age": 20,
+     "position":"Staff"
+ }
+
+}
