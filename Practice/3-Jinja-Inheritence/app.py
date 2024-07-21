f902cf7743de712666a3ddb2fed44b7954843360
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-07-21 19:18:56 +0500

    Add emp.html

--- Practice/3-Jinja-Inheritence/app.py
+++ Practice/3-Jinja-Inheritence/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from emp import employees_data
 
 app = Flask(__name__)
 
@@ -10,6 +11,11 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/emp")
+def emp():
+    return render_template("emp.html",title="Employees", data=employees_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
