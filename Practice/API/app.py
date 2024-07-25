e6583903f66079e045376e4bd6eb731a5b69a6ba
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-07-25 15:50:17 +0500

    Add script to handle post request

--- Practice/API/app.py
+++ Practice/API/app.py
@@ -27,7 +27,13 @@ def fail(score):
 def form():
     if request.method == "GET":
         return render_template("form.html")
+    else:
+        maths   = float(request.form["maths"])
+        science = float(request.form["science"])
+        history = float(request.form["history"])
 
+        average_marks = (maths+science+history)/3
+        return render_template("form.html",score=average_marks)
 
 if __name__=="__main__":
     app.run(debug=True)
