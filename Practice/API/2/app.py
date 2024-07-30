ba460c6c21cf55ea69c6c4803e6a8ffb8786e863
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-07-30 20:54:54 +0500

    Add code for PUT and DELETE methods

--- Practice/API/2/app.py
+++ Practice/API/2/app.py
@@ -73,9 +73,41 @@ def books():
         # return the updated list/in memory db in the form of json
         return jsonify(books_list), 201
     
+
+# GET | PUT | DELETE
+# Note: Previous route is books
+@app.route("/book/<int:id>",methods=["GET","PUT","DELETE"])
+def single_book(id):
+    if request.method=="GET":
+        for book in books_list:
+            if book['id']==id:
+                return jsonify(book)
+            
+
+    if request.method=='PUT':
+        for book in books_list:
+            if book['id']==id:
+                book['author']==request.form['author']
+                book['language']==request.form['language']
+                book['title']==request.form['title']
+                updated_book = {
+                    'id':id,
+                    'author':book['author'],
+                    'language':book['language'],
+                    'title':book['title'],
+                }        
+                return jsonify(updated_book)
+            
+    if request.method == "DELETE":
+         for index, book in enumerate(books_list):
+             if book['id']==id:
+                 books_list.pop(index)
+                 return jsonify(books_list)
+    
 if __name__=="__main__":
     app.run(debug=True)
 
 # copy the link and move towards postman
 # POST > Body > form-data> Key values> send> Yep! All done!
+
 
