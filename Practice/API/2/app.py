7ca679f080a39459de88bce4c804e0e7a03780bc
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-08-03 18:11:47 +0500

    Completed form

--- Practice/API/2/app.py
+++ Practice/API/2/app.py
@@ -1,7 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
+users = [
+    {
+        "name":"sagar",
+        "city":"kandhkot",
+        "dept":"cs",
+    }
+]
+
+
 @app.route("/")
 def index():
+    return render_template("index.html",title="index")
+
+@app.route("/form",methods=['GET','POST'])
+def form():
+    if request.method == "POST":
+        maths   = float(request.form['maths'])
+        english = float(request.form['english'])
+        ict     = float(request.form['ict'])
+
+        score = (maths + english + ict) / 3
+        return render_template("form.html",score=score,title="form")
+    
     return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
